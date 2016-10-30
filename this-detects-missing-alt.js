31f15156e367c807ab4cac737390d79592d7abda
Wojciech Frącz <fraczwojciech@gmail.com>
2016-10-30 21:12:06 +0100

    first commit

---
+++ this-detects-missing-alt.js
@@ -0,0 +1,12 @@
+var AccessSniff = require('access-sniff');
+
+AccessSniff
+  .default(['files/sample.html'], {
+      force: true, 
+      ignore: [
+        'WCAG2A.Principle2.Guideline2_4.2_4_2.H25.1.NoTitleEl',
+        'WCAG2A.Principle3.Guideline3_1.3_1_1.H57.2'
+      ]})
+  .then(function(report) {
+    AccessSniff.report(report, {});
+  });
